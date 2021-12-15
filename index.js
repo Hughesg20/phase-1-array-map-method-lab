4916fcd2651ea57817f7e91599298e3f5f6ca920
Griffin Hughes <Griffinhughes2020@gmail.com>
2021-12-15 18:01:10 -0500

    commit tree fc4923831a67cb98aef13154b1fc41cf44f92a4f

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
+//i need to write a function with a param of an array(tutorials) the returns the same array but with the first letter of each word capatilized
+// which means i have to map it and the once the function has an element it needs to delete the old one change the one it has to capatilized and then push the new one into the same position
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(words => words.split(' ').map(func => 
+    func.charAt(0).toUpperCase() + func.slice(1)).join(' '))
+  }
